8f227c26276f3e530bdc0581a341d78c3908845e
Jesse Sahli <jsahli@xogrp.com>
2018-07-26 11:55:23 -0400

    embedded guest VC view

--- KnotSeatingHackathon/ViewController.swift
+++ KnotSeatingHackathon/ViewController.swift
@@ -10,13 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var guestContianerView: UIView!
+    
+    @IBOutlet weak var guestContainerView: UIView!
     @IBOutlet weak var catalogueContainerView: UIView!
     @IBOutlet weak var floorPlanContainerView: UIView!
+    let guestsViewController = GuestsViewController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        addGuestsViewController()
+    }
+
+    func addGuestsViewController() {
+        addChildViewController(guestsViewController)
+        view.addSubview(guestsViewController.view)
+        guestsViewController.didMove(toParentViewController: self)
+        guestsViewController.view.translatesAutoresizingMaskIntoConstraints = false
+        guestsViewController.view.topAnchor.constraint(equalTo: self.guestContainerView.topAnchor).isActive = true
+        guestsViewController.view.bottomAnchor.constraint(equalTo: self.guestContainerView.bottomAnchor).isActive = true
+        guestsViewController.view.trailingAnchor.constraint(equalTo: self.guestContainerView.trailingAnchor).isActive = true
+        guestsViewController.view.leadingAnchor.constraint(equalTo: self.guestContainerView.leadingAnchor).isActive = true
     }
 }
+
+
 
