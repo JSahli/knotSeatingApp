0da08b423f3bbbcecaf683cbe65e2e0c790ba677
Kadell Gregory <kgregory@xogrp.com>
2018-07-26 16:45:28 -0400

    Add custom Wedding View

---
+++ KnotSeatingHackathon/Views/WeddingTableView.swift
@@ -0,0 +1,24 @@
+//
+//  WeddingTableView.swift
+//  KnotSeatingHackathon
+//
+//  Created by Kadell Gregory on 7/26/18.
+//  Copyright © 2018 XO Group. All rights reserved.
+//
+
+import UIKit
+
+class WeddingTableView: UIView {
+
+    @IBOutlet weak var tableNumberLabel: UILabel!
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
