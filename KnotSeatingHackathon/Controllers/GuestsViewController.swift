c0e634a941a1cf9bd28b73bc917f951f7cfd66b0
Jesse Sahli <jsahli@xogrp.com>
2018-07-27 09:38:43 -0400

    changed guestVC to tablview

--- KnotSeatingHackathon/Controllers/GuestsViewController.swift
+++ KnotSeatingHackathon/Controllers/GuestsViewController.swift
@@ -10,21 +10,51 @@ import UIKit
 
 class GuestsViewController: UIViewController {
 
-    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var tableView: UITableView!
+//    @IBOutlet weak var collectionView: UICollectionView!
     let testGuests = Guest.dummyGuests()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureCollectionView()
+//        configureCollectionView()
+        configureTableView()
     }
 
-    private func configureCollectionView() {
-        collectionView.dataSource = self
-        collectionView.register(GuestCollectionViewCell.nib, forCellWithReuseIdentifier: GuestCollectionViewCell.reuseId)
-        guard let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout else { return }
-        layout.itemSize = CGSize(width: collectionView.frame.width, height: 50)
-        collectionView.dragDelegate = self
-        collectionView.dragInteractionEnabled = true
+    private func configureTableView() {
+        tableView.dragInteractionEnabled = true
+        tableView.dataSource = self
+        tableView.dragDelegate = self
+    }
+
+//    private func configureCollectionView() {
+//        collectionView.dataSource = self
+//        collectionView.register(GuestCollectionViewCell.nib, forCellWithReuseIdentifier: GuestCollectionViewCell.reuseId)
+//        guard let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout else { return }
+//        layout.itemSize = CGSize(width: collectionView.frame.width, height: 50)
+//        collectionView.dragDelegate = self
+//        collectionView.dragInteractionEnabled = true
+//    }
+}
+
+extension GuestsViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return testGuests.count
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return testGuests[section].count
+    }
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return testGuests[section][0].group
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") ?? UITableViewCell(style: UITableViewCellStyle.subtitle, reuseIdentifier: "Cell")
+        let currentGuest = testGuests[indexPath.section][indexPath.row]
+        cell.textLabel?.text = currentGuest.fullName
+        cell.detailTextLabel?.text = currentGuest.group
+        return cell
     }
 }
 
@@ -40,30 +70,43 @@ extension GuestsViewController: UICollectionViewDataSource {
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: GuestCollectionViewCell.reuseId, for: indexPath) as? GuestCollectionViewCell else { return UICollectionViewCell() }
-        let currentGuest = testGuests[indexPath.row]
+        let currentGuest = testGuests[indexPath.section][indexPath.row]
         cell.nameLabel.text = currentGuest.fullName
         cell.groupNameLabel.text = currentGuest.group
         return cell
     }
 }
 
-extension GuestsViewController: UICollectionViewDragDelegate {
-    func collectionView(_ collectionView: UICollectionView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
-        session.localContext = collectionView
+//extension GuestsViewController: UICollectionViewDragDelegate {
+//    func collectionView(_ collectionView: UICollectionView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+//        session.localContext = collectionView
+//        return dragItems(at: indexPath)
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, itemsForAddingTo session: UIDragSession, at indexPath: IndexPath, point: CGPoint) -> [UIDragItem] {
+//        return dragItems(at: indexPath)
+//    }
+//}
+
+extension GuestsViewController: UITableViewDragDelegate {
+    func tableView(_ tableView: UITableView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+        session.localContext = tableView
         return dragItems(at: indexPath)
     }
 
-    func collectionView(_ collectionView: UICollectionView, itemsForAddingTo session: UIDragSession, at indexPath: IndexPath, point: CGPoint) -> [UIDragItem] {
+    func tableView(_ tableView: UITableView, itemsForAddingTo session: UIDragSession, at indexPath: IndexPath, point: CGPoint) -> [UIDragItem] {
+        session.localContext = tableView
         return dragItems(at: indexPath)
     }
+    
 }
 
 // MARK: Helper Functions
 extension GuestsViewController {
     private func dragItems(at indexPath: IndexPath) -> [UIDragItem] {
-        if let image = (collectionView.cellForItem(at: indexPath) as? GuestCollectionViewCell)?.nameLabel.text{
-            let dragItem = UIDragItem(itemProvider: NSItemProvider(object: image as NSItemProviderWriting))
-            dragItem.localObject = testGuests[indexPath.row]
+        if let name = (tableView.cellForRow(at: indexPath))?.textLabel?.text{
+            let dragItem = UIDragItem(itemProvider: NSItemProvider(object: name as NSItemProviderWriting))
+            dragItem.localObject = testGuests[indexPath.section][indexPath.row]
             return [dragItem]
         } else {
             return []
--- KnotSeatingHackathon/Models/Guest.swift
+++ KnotSeatingHackathon/Models/Guest.swift
@@ -16,8 +16,8 @@ class Guest: NSObject {
         self.group = group
     }
 
-    static func dummyGuests() -> [Guest] {
-        let guests =
+    static func dummyGuests() -> [[Guest]] {
+        let guests1 =
         [Guest(firstName: "Hamid", lastName: "Tavakoli", group: "iOS Friends"),
          Guest(firstName: "Ben", lastName: "Mayer", group: "iOS Friends"),
          Guest(firstName: "Jen", lastName: "Sipila", group: "iOS Friends"),
@@ -26,20 +26,22 @@ class Guest: NSObject {
          Guest(firstName: "George", lastName: "Correa", group: "iOS Friends"),
          Guest(firstName: "Daniel", lastName: "Burbank", group: "iOS Friends"),
          Guest(firstName: "Stephen", lastName: "Bradley", group: "iOS Friends"),
-         Guest(firstName: "Krishna", lastName: "Ramachandran", group: "iOS Friends"),
-         Guest(firstName: "Felix", lastName: "Gregory", group: "Family"),
-         Guest(firstName: "Bridget", lastName: "Gregory", group: "Friends"),Guest(firstName: "John", lastName: "Harte", group: "Family"),
-         Guest(firstName: "Cara", lastName: "Harte", group: "Family"),
-         Guest(firstName: "Diane", lastName: "Gregory", group: "Family"),
-         Guest(firstName: "Gaia", lastName: "Claudel", group: "Friends"),
-         Guest(firstName: "Karl", lastName: "Sredl", group: "Friends"),
-         Guest(firstName: "Mario", lastName: "Johnson", group: "Friends"),
-         Guest(firstName: "Roxy", lastName: "Morganella", group: "Friends"),
-         Guest(firstName: "Rufus", lastName: "Sabatello", group: "Intramural team"),
-         Guest(firstName: "Zeb", lastName: "Harbron", group: "Intramural team"),
-         Guest(firstName: "York", lastName: "Berson", group: "Intramural team"),
-         Guest(firstName: "Mimi", lastName: "Kotter", group: "Intramural team")
-         ]
-        return guests
+        Guest(firstName: "Krishna", lastName: "Ramachandran", group: "iOS Friends")]
+        let guests2 = [Guest(firstName: "Felix", lastName: "Gregory", group: "Family"),
+                       Guest(firstName: "John", lastName: "Harte", group: "Family"),
+                       Guest(firstName: "Cara", lastName: "Harte", group: "Family"),
+                       Guest(firstName: "Diane", lastName: "Gregory", group: "Family")]
+
+        let guests3 = [Guest(firstName: "Gaia", lastName: "Claudel", group: "Friends"),
+                                Guest(firstName: "Karl", lastName: "Sredl", group: "Friends"),
+                                Guest(firstName: "Mario", lastName: "Johnson", group: "Friends"),
+                                Guest(firstName: "Bridget", lastName: "Gregory", group: "Friends"),
+                                Guest(firstName: "Roxy", lastName: "Morganella", group: "Friends")]
+        let guests4 = [         Guest(firstName: "Rufus", lastName: "Sabatello", group: "Intramural team"),
+                                Guest(firstName: "Zeb", lastName: "Harbron", group: "Intramural team"),
+                                Guest(firstName: "York", lastName: "Berson", group: "Intramural team"),
+                                Guest(firstName: "Mimi", lastName: "Kotter", group: "Intramural team")
+        ]
+        return [guests1, guests2, guests3, guests4]
     }
 }
